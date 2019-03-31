dfa2b7720be1eac1b0161659a565326a1743c867
aljoker2001 <aljoker2001@hotmail.com>
2019-03-30 21:24:25 -0700

    it works!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var timer = 30;
+var timer = 20;
 var start = document.querySelector("#start");
 var triviaField = document.createElement("div");
 var startButton = document.createElement("button");
@@ -10,7 +10,11 @@ var questions = [
     { question: "Who is Harry's giant friend?", answers: ["Firenze", "Buckbeak", "Scabbers", "Hagrid"] },
     { question: "What is the mascot of Slytherin House?", answers: ["Griffin", "Raven", "Badger", "Snake"] },
     { question: "When was Harry Potter born?", answers: ["October 31, 1980", "August 31, 1980", "May 31, 1980", "July 31, 1980"] },
-    { question: "What wasn't a task in the Tri-Wizard tournament?", answers: ["Fight a dragon", "Find a friend in the Black lake", "Hedge Maze", "Win at Wizard's Chess"] }
+    { question: "What wasn't a task in the Tri-Wizard tournament?", answers: ["Fight a dragon", "Find a friend in the Black lake", "Hedge Maze", "Win at Wizard's Chess"] },
+    { question: "What is Harry Potter's patronus charm?", answers: ["Hare", "Penguin", "Otter", "Stag"] },
+    { question: "Who did not teach Defense against the Dark Arts?", answers: ["Severus Snape", "Albus Dumbledore", "Remus Lupin", "Alastor Moody"] },
+    { question: "Who of the following is not an Animagus?", answers: ["Minerva McGonagall", "Sirius Black", "Peter Pettigrew", "Sybill Trelawney"]},
+    { question: "What was the name of Albus Dumbledore's sister?", answers: ["Aliana", "Athiana", "Astrina", "Ariana"]}
 ];
 var askedQuestions = [];
 var timerID;
@@ -24,7 +28,10 @@ var shuffledOptions = [];
 var rightWrong;
 var answers = [];
 var reset;
+var correctField;
+var incorrectField;
 var playAgain;
+var t = [1, 2, 3, 5, 6]
 
 // Starts game with first question and timer upon pressing the "Start" button.
 var startGame = () => {
@@ -35,7 +42,9 @@ var startGame = () => {
     tp.textContent = `Time Remaining: ${timer}`;
     triviaField.classList.add("triviaField");
     wrapper.appendChild(triviaField);
-    wrapper.removeChild(start);
+    if (shuffledOptions.length === 0) {
+        wrapper.removeChild(start);
+    }
     qp.classList.add("questionPara");
     qp.textContent = question;
     triviaField.appendChild(qp);
@@ -48,12 +57,22 @@ var startGame = () => {
 var newQuestion = () => {
     // console.log(timerID);
     clearInterval(timerID);
-    clear();
     // Gives option to play again once all questions are asked
     if (askedQuestions.length === questions.length) {
+        clear();
+        askedQuestions = [];
+        triviaField.removeChild(rightWrong);
+        correctField = document.createElement("h2")
+        correctField.textContent = `Correct: ${correct}`;
+        correctField.setAttribute("class", "totals");
+        triviaField.appendChild(correctField);
+        incorrectField = document.createElement("h2")
+        incorrectField.textContent = `Incorrect: ${incorrect}`;
+        incorrectField.setAttribute("class", "totals");
+        triviaField.appendChild(incorrectField);
         playAgain = document.createElement("h3");
         playAgain.setAttribute("id", "playAgain");
-        playAgain.textContent = "Would you like to play again?"
+        playAgain.textContent = "Click to play again"
         triviaField.appendChild(playAgain);
         reset = document.getElementById("playAgain");
         reset.addEventListener("click", startOver);
@@ -61,17 +80,18 @@ var newQuestion = () => {
     }
     position = Math.floor(Math.random() * questions.length);
     question = questions[position].question;
+    getAnswers();
     // Displays a new question and resets the timer
     if (askedQuestions.indexOf(question) === -1) {
-        timer = 30;
+        timer = 20;
         tp.textContent = `Time Remaining: ${timer}`;
         askedQuestions.push(question);
         qp.textContent = question;
-        getAnswers();
         timerID = setInterval(count, 1000);
         console.log(question);
-    // Gets a new question if the randomly selected question has already been asked
+        // Gets a new question if the randomly selected question has already been asked
     } else {
+        clear();
         newQuestion();
     }
 }
@@ -88,6 +108,12 @@ var count = () => {
 
 // Puts answers for the respective question into the triviaField element
 var getAnswers = () => {
+    if (options.length !== 0) {
+        clear();
+    }
+    if (triviaField.childNodes.length > 2 && askedQuestions.length !== questions.length) {
+        triviaField.removeChild(rightWrong);
+    }
     questions[position].answers.forEach((ans) => {
         options.push(ans);
     });
@@ -108,8 +134,10 @@ var getAnswers = () => {
 
 // Once all questions have been asked, this resets the game if they would like to play again
 var startOver = () => {
-    wrapper.removeChild(playAgain);
-    askedQuestions = [];
+    triviaField.removeChild(playAgain);
+    triviaField.removeChild(correctField);
+    triviaField.removeChild(incorrectField);
+    // askedQuestions = [];
     startGame();
 }
 
@@ -144,13 +172,18 @@ var isAnswer = function () {
 // clears out old answers, resets timer, and clears the timerID interval
 var clear = () => {
     clearInterval(timerID);
-    timer = 30;
+    timer = 20;
     options = [];
     for (i = 0; i < 4; i++) {
         var b = answers.length - 1;
-        triviaField.removeChild(answers[b]);
+        var alist = Object.values(triviaField.childNodes);
+        if (alist.indexOf(answers[b])) {
+            triviaField.removeChild(answers[b]);
+        } else {
+            console.log(triviaField.childNodes);
+        }
     }
-    triviaField.removeChild(rightWrong);
+    // triviaField.removeChild(rightWrong);
 }
 
 // Shuffles the order of the answers so the correct answer isn't always the bottom right option
